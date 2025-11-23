4f8888a1d3493ba0ae8f107784424f1a7262a951
srinivasyanamandratechnum <srinivasyanamandra@technumopus.com>
2025-11-23 12:33:08 +0530

    lastest commit

---
+++ src/main/java/syncqubits/ai/blog/pranuBlog/PranuBlogApplication.java
@@ -0,0 +1,13 @@
+package syncqubits.ai.blog.pranuBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PranuBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PranuBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/syncqubits/ai/blog/pranuBlog/PranuBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package syncqubits.ai.blog.pranuBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PranuBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
